2fa1af0882af23ca4a706d0f1dd84d461fefdd6b
seifeldin.io <76487753+seifeldinio@users.noreply.github.com>
2024-05-09 09:33:32 +0300

    update package.json, src/hooks/card/index.ts and src/hooks/wallet/index.ts

--- src/hooks/card/index.ts
+++ src/hooks/card/index.ts
@@ -1,79 +1,214 @@
-import {
-  getAuthToken,
-  createOrder,
-  getFinalToken,
-} from "@/utils/payment-utils";
-import { Address } from "@/types";
-
-export const startCardProcess = async ({
-  amount,
-  currency,
-  name,
-  description,
-  quantity,
-  email, // Include email in function parameters
-  phoneNumber, // Include phoneNumber in function parameters
-  address,
-  paymobApiKey,
-  cardIntegrationId,
-  iframeId,
-  city,
-}: {
-  amount: number;
-  currency: string;
-  name: string;
-  description: string;
-  quantity: string;
-  email: string; // Define type for email
-  phoneNumber: string; // Define type for phoneNumber
-  address: Address;
-  paymobApiKey: string;
-  cardIntegrationId: string;
-  iframeId: string;
-  city: string;
-}): Promise<void> => {
+// STEP ONE
+export const startCardProcess = async (
+  amount: number,
+  currency: string,
+  courseTitle: string,
+  courseDescription: string,
+  firstName: string,
+  lastName: string,
+  email: string,
+  phoneNumber: string,
+  userId: number,
+  courseId: number,
+  paymobApiKey: string,
+  cardIntegrationId: number,
+  iframeId: number
+): Promise<void> => {
   try {
-    const token = await getAuthToken(paymobApiKey);
-    const orderId = await createOrder({
-      token,
-      amount,
-      currency,
-      name,
-      description,
-      quantity,
-      email, // Pass email to createOrder
-      phoneNumber, // Pass phoneNumber to createOrder
-      address,
+    let data = {
+      api_key: paymobApiKey,
+    };
+
+    let request = await fetch("https://accept.paymob.com/api/auth/tokens", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
     });
 
-    const finalToken = await getFinalToken({
-      token,
-      orderId,
-      amount,
-      currency,
-      email,
-      phoneNumber,
-      cardIntegrationId,
-      address,
-      city,
-    });
+    let response = await request.json();
 
-    cardPayment(iframeId, finalToken);
+    let token = response.token;
+
+    if (token) {
+      await secondStep({
+        token,
+        amount,
+        currency,
+        courseTitle,
+        courseDescription,
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        userId,
+        courseId,
+        cardIntegrationId,
+        iframeId,
+      });
+    }
   } catch (error) {
-    console.error("Error starting card payment process:", error);
-    throw new Error("Failed to start card payment process");
+    console.error("Error in startCardProcess:", error);
   }
 };
 
+// STEP TWO
+export const secondStep = async ({
+  token,
+  amount,
+  currency,
+  courseTitle,
+  courseDescription,
+  firstName,
+  lastName,
+  email,
+  phoneNumber,
+  userId,
+  courseId,
+  cardIntegrationId,
+  iframeId,
+}: {
+  token: string;
+  amount: number;
+  currency: string;
+  courseTitle: string;
+  courseDescription: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  userId: number;
+  courseId: number;
+  cardIntegrationId: number;
+  iframeId: number;
+}): Promise<void> => {
+  try {
+    let data = {
+      auth_token: token,
+      delivery_needed: "false",
+      amount_cents: (amount * 100).toString(),
+      currency,
+      items: [
+        {
+          name: courseTitle,
+          amount_cents: (amount * 100).toString(),
+          description: `Ends at ${courseDescription}`,
+          quantity: "1",
+        },
+      ],
+      shipping_data: {
+        first_name: firstName,
+        last_name: lastName,
+        email,
+        phone_number: phoneNumber,
+        extra_description: userId.toString(),
+        building: courseId,
+        city: courseDescription,
+        floor: courseTitle.toString(),
+        postal_code: "",
+      },
+    };
+
+    let request = await fetch(
+      "https://accept.paymob.com/api/ecommerce/orders",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      }
+    );
+
+    let response = await request.json();
+
+    let id = response?.id;
+
+    if (id) {
+      await thirdStep(
+        token,
+        id,
+        amount,
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        currency,
+        cardIntegrationId,
+        iframeId
+      );
+    }
+  } catch (error) {
+    console.error("Error in secondStep:", error);
+  }
+};
+
+// STEP THREE
+export const thirdStep = async (
+  token: string,
+  id: number,
+  amount: number,
+  firstName: string,
+  lastName: string,
+  email: string,
+  phoneNumber: string,
+  currency: string,
+  cardIntegrationId: number,
+  iframeId: number
+): Promise<void> => {
+  try {
+    let data = {
+      auth_token: token,
+      amount_cents: (amount * 100).toString(),
+      expiration: 3600,
+      order_id: id,
+      billing_data: {
+        first_name: firstName,
+        last_name: lastName,
+        email,
+        phone_number: phoneNumber,
+        apartment: "803",
+        floor: "42",
+        street: "Ethan Land",
+        building: "8028",
+        shipping_method: "PKG",
+        postal_code: "01898",
+        city: "Jaskolskiburgh",
+        country: "CR",
+        state: "Utah",
+      },
+      currency,
+      integration_id: cardIntegrationId,
+    };
+
+    let request = await fetch(
+      "https://accept.paymob.com/api/acceptance/payment_keys",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      }
+    );
+
+    let response = await request.json();
+
+    let finalToken = response.token;
+
+    if (finalToken) {
+      await cardPayment(iframeId, finalToken);
+    }
+  } catch (error) {
+    console.error("Error in thirdStep:", error);
+  }
+};
+
+// CARD PAYMENT
 export const cardPayment = async (
-  iframeId: string,
+  iframeId: number,
   finalToken: string
 ): Promise<void> => {
   try {
-    const iframeURL = `https://accept.paymob.com/api/acceptance/iframes/${iframeId}?payment_token=${finalToken}`;
-    window.location.href = iframeURL;
+    let iframeURL = `https://accept.paymob.com/api/acceptance/iframes/${iframeId}?payment_token=${finalToken}`;
+    // Redirect to the Paymob payment iframe URL
+    location.href = iframeURL;
   } catch (error) {
-    console.error("Error redirecting to card payment:", error);
-    throw new Error("Failed to redirect to card payment");
+    console.error("Error in cardPayment:", error);
   }
 };
--- src/hooks/wallet/index.ts
+++ src/hooks/wallet/index.ts
@@ -1,16 +1,199 @@
-import {
-  getAuthToken,
-  createOrder,
-  getFinalToken,
-} from "@/utils/payment-utils";
-import { Address } from "@/types";
+export const startWalletProcess = async (
+  amount: number,
+  currency: string,
+  courseTitle: string,
+  courseDescription: string,
+  firstName: string,
+  lastName: string,
+  email: string,
+  phoneNumber: string,
+  userId: number,
+  courseId: number,
+  paymobApiKey: string,
+  walletIntegrationId: number,
+  iframeId: number,
+  mobileNumber: string
+): Promise<void> => {
+  try {
+    let data = {
+      api_key: paymobApiKey,
+    };
+
+    let request = await fetch("https://accept.paymob.com/api/auth/tokens", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+    let response = await request.json();
+
+    let token = response.token;
+
+    if (token) {
+      walletSecondStep(
+        token,
+        amount,
+        currency,
+        courseTitle,
+        courseDescription,
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        userId,
+        courseId,
+        walletIntegrationId,
+        iframeId,
+        mobileNumber
+      );
+    }
+  } catch (error) {
+    console.error("Error in startWalletProcess:", error);
+  }
+};
+
+export const walletSecondStep = async (
+  token: string,
+  amount: number,
+  currency: string,
+  courseTitle: string,
+  courseDescription: string,
+  firstName: string,
+  lastName: string,
+  email: string,
+  phoneNumber: string,
+  userId: number,
+  courseId: number,
+  walletIntegrationId: number,
+  iframeId: number,
+  mobileNumber: string
+): Promise<void> => {
+  try {
+    let data = {
+      auth_token: token,
+      delivery_needed: "false",
+      amount_cents: (amount * 100).toString(),
+      currency,
+      items: [
+        {
+          name: courseTitle,
+          amount_cents: (amount * 100).toString(),
+          description: `Ends at ${courseDescription}`,
+          quantity: "1",
+        },
+      ],
+      shipping_data: {
+        first_name: firstName,
+        last_name: lastName,
+        email,
+        phone_number: phoneNumber,
+        extra_description: userId.toString(),
+        building: courseId,
+        city: courseDescription,
+        floor: courseTitle.toString(),
+        postal_code: "",
+      },
+    };
+
+    let request = await fetch(
+      "https://accept.paymob.com/api/ecommerce/orders",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      }
+    );
+
+    let response = await request.json();
+
+    let id = response?.id;
+
+    if (id) {
+      walletThirdStep(
+        token,
+        id,
+        amount,
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        currency,
+        walletIntegrationId,
+        iframeId,
+        mobileNumber
+      );
+    }
+  } catch (error) {
+    console.error("Error in secondStep:", error);
+  }
+};
+
+export const walletThirdStep = async (
+  token: string,
+  id: number,
+  amount: number,
+  firstName: string,
+  lastName: string,
+  email: string,
+  phoneNumber: string,
+  currency: string,
+  walletIntegrationId: number,
+  iframeId: number,
+  mobileNumber: string
+): Promise<void> => {
+  try {
+    let data = {
+      auth_token: token,
+      amount_cents: (amount * 100).toString(),
+      expiration: 3600,
+      order_id: id,
+      billing_data: {
+        first_name: firstName,
+        last_name: lastName,
+        email,
+        phone_number: phoneNumber,
+        apartment: "803",
+        floor: "42",
+        street: "Ethan Land",
+        building: "8028",
+        shipping_method: "PKG",
+        postal_code: "01898",
+        city: "Jaskolskiburgh",
+        country: "CR",
+        state: "Utah",
+      },
+      currency,
+      integration_id: walletIntegrationId,
+    };
+
+    let request = await fetch(
+      "https://accept.paymob.com/api/acceptance/payment_keys",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      }
+    );
+
+    let response = await request.json();
+
+    let finalToken = response.token;
+
+    if (finalToken) {
+      walletPayment(iframeId, finalToken, mobileNumber);
+    }
+  } catch (error) {
+    console.error("Error in thirdStep:", error);
+  }
+};
 
 export const walletPayment = async (
+  iframeId: number,
   finalToken: string,
   mobileNumber: string
 ): Promise<void> => {
   try {
-    const response = await fetch(
+    let request = await fetch(
       "https://accept.paymob.com/api/acceptance/payments/pay",
       {
         method: "POST",
@@ -25,82 +208,14 @@ export const walletPayment = async (
       }
     );
 
-    const responseData = await response.json();
+    console.log(iframeId);
 
-    if (responseData.redirect_url) {
-      window.location.href = responseData.redirect_url;
-    }
+    let response = await request.json();
+
+    const iframeURL = response.redirect_url;
+
+    location.href = iframeURL;
   } catch (error) {
-    console.error("Error redirecting to wallet payment:", error);
-    throw error;
-  }
-};
-
-interface WalletProcessArgs {
-  amount: number;
-  currency: string;
-  name: string;
-  description: string;
-  quantity: string;
-  email: string;
-  phoneNumber: string;
-  city: string;
-  paymobApiKey: string;
-  cardIntegrationId: string;
-  address: Address;
-  mobileNumber: string;
-}
-
-export const startWalletProcess = async ({
-  amount,
-  currency,
-  name,
-  description,
-  quantity,
-  email,
-  phoneNumber,
-  city,
-  paymobApiKey,
-  cardIntegrationId,
-  address,
-  mobileNumber,
-}: WalletProcessArgs): Promise<void> => {
-  try {
-    const token = await getAuthToken(paymobApiKey);
-
-    if (token) {
-      const orderId = await createOrder({
-        token,
-        amount,
-        currency,
-        name,
-        description,
-        quantity,
-        email,
-        phoneNumber,
-        address, // Pass the address object directly
-      });
-
-      if (orderId) {
-        const finalToken = await getFinalToken({
-          token,
-          orderId,
-          amount,
-          currency,
-          email,
-          phoneNumber,
-          cardIntegrationId,
-          address, // Pass the address object directly
-          city, // Include the city parameter
-        });
-
-        if (finalToken) {
-          await walletPayment(finalToken, mobileNumber);
-        }
-      }
-    }
-  } catch (error) {
-    console.error("Error starting wallet payment process:", error);
-    throw error;
+    console.error("Error in walletPayment:", error);
   }
 };
